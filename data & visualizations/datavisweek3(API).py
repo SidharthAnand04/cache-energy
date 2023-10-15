55bee5382592c014b5e8e382d2c7984fbaf54799
archit4illinois <129916007+archit4illinois@users.noreply.github.com>
2023-10-14 21:10:05 -0500

    Update datavisweek3(API).py

--- data & visualizations/datavisweek3(API).py
+++ data & visualizations/datavisweek3(API).py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 import requests
+from matplotlib.ticker import ScalarFormatter  
+
 
 api_url = "https://api.eia.gov/v2/electricity/rto/region-data/data/?api_key=Sr32RhpckMKBDCIz8HzmaB0Y7dKDW0T90edtf0rN&frequency=hourly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
 
@@ -41,12 +43,16 @@ else:
     print("Failed to retrieve data. Status code: {response.status_code}")
     print(response.status_code)
     quit()
-    
-values = [entry["value"] for entry in data["response"]["data"]]
-times = [entry["period"] for entry in data["response"]["data"]]
+
+filtered_data = [(entry['period'], entry['value']) for entry in data['response']['data'] if isinstance(entry['value'], int) and entry['value'] > 300000]
+times, values = zip(*filtered_data)
+
+
+'''values = [entry["value"] for entry in data["response"]["data"] if isinstance(entry['value'], int) and entry['value'] > 300000]
+times = [entry["period"] for entry in data["response"]["data"]]'''
 
 times = [pd.to_datetime(time) for time in times]
-
+print(filtered_data)
 plt.figure(figsize=(10, 6))
 plt.scatter(times, values, marker='o', color='b')
 plt.title("Electricity Demand")
@@ -54,6 +60,9 @@ plt.xlabel("Period (MM-DD Hour)")
 plt.ylabel("Demand (megawatthours)")
 plt.xticks(rotation=45)
 plt.grid(True)
+formatter = ScalarFormatter()
+formatter.set_scientific(False)
+plt.gca().get_yaxis().set_major_formatter(formatter)
 
 plt.tight_layout()
 plt.show()
