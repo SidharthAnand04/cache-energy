66697d685d10fe70cf8760c7dc1cbf0c21148f4d
Ashley <ashleyli@wirelessprv-10-195-94-200.near.illinois.edu>
2023-10-23 16:50:46 -0500

    inital commit

---
+++ app/src/App.js
@@ -0,0 +1,29 @@
+import dog from './dog.png';
+import './App.css';
+
+function App() {
+  return (
+    <div></div>
+
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={dog} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"S
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
